9a4e77f5b0d437eb56cad9cc9ded472d62391e52
dmora <dmora@ciudaddemascotas.com>
2022-03-01 14:00:08 -0500

    Add urls core

--- crud/urls.py
+++ crud/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 
 urlpatterns = [
+    
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
