c779152c19ae9455ab0dfb28fda5371a6f3e7c5d
dmora <dmora@ciudaddemascotas.com>
2022-03-04 11:40:20 -0500

    Add pages modified

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,20 @@
 from pyexpat import model
 from sre_constants import SUCCESS
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.urls import reverse , reverse_lazy
 from django.views.generic.edit import CreateView , UpdateView , DeleteView
 from .forms import PageForm
+from django.shortcuts import redirect
+
+class StaffRequiredMixin(object):
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request , *args , **kwargs )
 
 # Create your views here.
 
@@ -16,13 +24,13 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreateView(CreateView):
+class PageCreateView(StaffRequiredMixin ,CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy ('pages:pages')
 
 
-class PageUpdateView(UpdateView):
+class PageUpdateView(StaffRequiredMixin ,UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
@@ -31,7 +39,7 @@ class PageUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:updated', args=[self.object.id]) + '?ok'
 
-class PageDeleteView(DeleteView):
+class PageDeleteView(StaffRequiredMixin,DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
 
