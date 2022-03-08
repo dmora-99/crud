6c883961cb7dba191340247a8e1cd9069be97e41
dmora <dmora@ciudaddemascotas.com>
2022-03-08 11:22:51 -0500

    Validation email exist

--- registration/forms.py
+++ registration/forms.py
@@ -13,3 +13,9 @@ class UserCreationFormWithEmail (UserCreationForm):
         model = User
         fields = ("username","email","password1","password2")
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email= email).exists():
+            raise forms.ValidationError("El email ya existe")
+        return email
+
